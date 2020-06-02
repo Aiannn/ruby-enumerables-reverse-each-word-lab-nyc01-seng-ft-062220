8583b6d45b0be762780f2ed896bfad4d94ae4a80
Aian Soktoev <soktoevayan@gmail.com>
2020-06-02 01:27:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-string = "Hello how are you?"
+#string = "Hello how are you?"
 def reverse_each_word(string)
   string.split
   newArr = []
   string.split.collect do |element|
     newArr.push(element.reverse)
   end 
-  puts newArr.join(' ')
+  newArr.join(' ')
 end   
   
-reverse_each_word(string)  
+#reverse_each_word(string)  
