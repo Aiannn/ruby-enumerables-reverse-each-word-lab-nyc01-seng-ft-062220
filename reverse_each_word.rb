5edee740638e0e5017719c406767bbd9f7ad95c2
Aian Soktoev <soktoevayan@gmail.com>
2020-06-02 01:15:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,9 @@
 def reverse_each_word(string)
   string.split
   string.split.collect do |element|
-    a=element.reverse
+    a = element.reverse
   end 
+  a.join(' ')
 end   
   
 #reverse_each_word(string)  
